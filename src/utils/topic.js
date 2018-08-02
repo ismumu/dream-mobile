eb205a8b88cc8879eb8689e4a80510bc58ddc900
ismumu <ismmm@qq.com>
2018-08-02 14:44:54 +0800

    add

--- src/utils/topic.js
+++ src/utils/topic.js
@@ -3,19 +3,25 @@
 module.exports = {
 	initList(list, kw) {
 		return list.map((i) => {
-			if (i.content) {
-				i.content = i.content.replace(/(^|.+?)\#([^\<\>\s\n]+)/g, (a, b, c) => {
-					let mstr = c.replace('&nbsp;', '').replace(/^\s+|\s+$/, '');
-					let act = (kw && kw == mstr) ? true : false;
-					return b + '<a href="javascript:;" to="' + (act ? 'javascript:;' : '/topic?text=' + mstr) + '" class="topic J-topic' + (act ? ' active' : '') + '">#' + mstr + '</a>';
-				});
+			// if (i.content) {
+			// 	i.content = i.content.replace(/(^|.+?)\#([^\<\>\s\n]+)/g, (a, b, c) => {
+			// 		let mstr = c.replace('&nbsp;', '').replace(/^\s+|\s+$/, '');
+			// 		let act = (kw && kw == mstr) ? true : false;
+			// 		return b + '<a href="javascript:;" to="' + (act ? 'javascript:;' : '/topic?text=' + mstr) + '" class="topic J-topic' + (act ? ' active' : '') + '">#' + mstr + '</a>';
+			// 	});
+			// }
+			// if (i.review_content) {
+			// 	i.review_content = i.review_content.replace(/(^|.+?)\#([^\<\>\s\n]+)/g, (a, b, c) => {
+			// 		let mstr = c.replace(/^\s+|\s+$/, '');
+			// 		let act = (kw && kw == mstr) ? true : false;
+			// 		return b + '<a  href="javascript:;" to="' + (act ? 'javascript:;' : '/topic?text=' + mstr) + '" class="topic  J-topic' + (act ? ' active' : '') + '">#' + mstr + '</a>';
+			// 	});
+			// }
+			if ( i.content ) {
+				i.content = i.content.replace(/(?:#([^#\s]+)\s+([^#\s]+))/g, '<a href="javascript:;" class="J-topic" to="/topic?text=$1">#$1 </a>$2');
 			}
-			if (i.review_content) {
-				i.review_content = i.review_content.replace(/(^|.+?)\#([^\<\>\s\n]+)/g, (a, b, c) => {
-					let mstr = c.replace(/^\s+|\s+$/, '');
-					let act = (kw && kw == mstr) ? true : false;
-					return b + '<a  href="javascript:;" to="' + (act ? 'javascript:;' : '/topic?text=' + mstr) + '" class="topic  J-topic' + (act ? ' active' : '') + '">#' + mstr + '</a>';
-				});
+			if ( i.review_content ) {
+				i.review_content = i.review_content.replace(/(?:#([^#\s]+)\s+([^#\s]+))/g, '<a href="javascript:;" class="J-topic" to="/topic?text=$1">#$1 </a>$2');
 			}
 			return i;
 		});
