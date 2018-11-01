2e95ca3fcfecf188c7f56f2a473111d6fff40171
Ethan <lauethan@hotmail.com>
2018-11-01 17:20:32 +0800

    ethan

--- src/routes/my/dreamHints.js
+++ src/routes/my/dreamHints.js
@@ -15,9 +15,21 @@ export default class DreamHints extends React.Component {
 			<div>
 				<NavBarPage title="解梦提示" iconType="back" />
 				<div className={styles.dreamHints}>
-				<p>我们正在筛选一些学科上的建议给大家揭开自己的一些迷雾</p>
-				<p>揭开自己的一些迷雾</p>
-				揭开自己的一些迷雾
+				<br></br>
+				<br></br>
+				<br></br>
+				<p>我们正在筛选一些学科上的建议</p>
+				<br></br>
+				<p>给大家借鉴</p>
+				<br></br>
+				<p>用来揭开自己的一些迷雾</p>
+				<br></br>
+				<br></br>
+
+				<p>【Dreamers need to stick together】</p>
+				<p>find the ones who haven't given up</p>
+				<p>they are the future</p>
+
 				</div>
 			</div>
 		)
